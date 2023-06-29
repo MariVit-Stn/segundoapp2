358e5fd34be215a8c2244637584f8909c8753207
MariVit_Stn <mariavitoria.santana.ada@gmail.com>
2023-06-29 16:40:58 -0300

    Initial Commit

---
+++ segundoapp2/segundoapp2App.swift
@@ -0,0 +1,17 @@
+//
+//  segundoapp2App.swift
+//  segundoapp2
+//
+//  Created by Maria Vitória de Santana Gabriel on 29/06/23.
+//
+
+import SwiftUI
+
+@main
+struct segundoapp2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
